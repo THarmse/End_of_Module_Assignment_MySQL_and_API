35ccdf874cfc0153620d5a20436c7cc89591b7ee
TheodorHarmse <t.harmse@liverpool.ac.uk>
2023-12-09 18:31:36 +0400

    Theodor Harmse: Updated Title and Description of Index (View) to just indicate that the API is running.

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Hello World!' });
+    res.render('index', { title: 'University of Liverpool' });
 });
 
 module.exports = router;
