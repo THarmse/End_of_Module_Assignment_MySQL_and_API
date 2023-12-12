26dc9281fcef10aecce65f10a61f48b91415e664
TheodorHarmse <t.harmse@liverpool.ac.uk>
2023-12-12 17:27:18 +0400

    Theodor Harmse (Teddy):  Peer Review - Added Row count, added PassStatus as a variable, using sp: to exit, to optimize speed. added prefix to parameters and storedprocedure name update.

--- database scripts/Create Scripts/sp_assignStudentGrade.sql
+++ database scripts/Create Scripts/sp_assignStudentGrade.sql
@@ -1,34 +1,52 @@
-DELIMITER //
+-- Stored Procedure to Update Student's Pass/Fail Grade Status
+-- Version: 1.0
+-- Date Updated: 12 December 2023
+-- Author: Yumi 
+-- Group: Group C
 
-CREATE PROCEDURE `UpdateStudentStatus` (
-    IN p_TeacherID INT,
+USE `mydb`;
+
+DELIMITER $$
+
+-- Drop existing procedure if it exists
+DROP PROCEDURE IF EXISTS `sp_AssignStudentGrade`$$
+
+-- Create the new procedure
+CREATE PROCEDURE `sp_AssignStudentGrade`(
+    IN p_TeacherID INT, 
     IN p_StudentID INT,
-    IN p_PassStatus BOOLEAN
+    IN p_PassStatus BOOLEAN,
+    OUT p_AffectedRows INT
 )
-BEGIN
+sp:BEGIN
     DECLARE teacherIsTeacher INT;
     DECLARE studentExists INT;
+
+    -- Initialize p_AffectedRows
+    SET p_AffectedRows = 0;
 
     -- Check if the user is a teacher
     SELECT COUNT(*) INTO teacherIsTeacher
     FROM users
-    WHERE UserID = p_TeacherID AND RoleID = 2;
-    
-    -- Ensure the user is a teacher
+    WHERE UserID = p_TeacherID AND RoleID = 2; --  Teacher Role ID is 2
+
+    -- If the user is not a teacher, raise an error
     IF teacherIsTeacher = 0 THEN
         SIGNAL SQLSTATE '45000'
-        SET MESSAGE_TEXT = 'Only Teachers can update student status';
+        SET MESSAGE_TEXT = 'Transaction Error: Only teachers can update student grades.';
+        LEAVE sp;
     END IF;
 
     -- Check if the student exists
     SELECT COUNT(*) INTO studentExists
     FROM users
-    WHERE UserID = p_StudentID AND RoleID = 3;
+    WHERE UserID = p_StudentID AND RoleID = 3; -- Student Role ID is 3
 
     -- If the student does not exist, raise an error
     IF studentExists = 0 THEN
         SIGNAL SQLSTATE '45000'
-        SET MESSAGE_TEXT = 'Student does not exist';
+        SET MESSAGE_TEXT = 'Transaction Error: Student does not exist.';
+        LEAVE sp;
     END IF;
 
     -- Update the student status
@@ -36,12 +54,14 @@ BEGIN
     SET Mark = p_PassStatus
     WHERE UserID = p_StudentID;
 
-    IF p_PassStatus THEN
-        SELECT 'Student passed' AS Result;
-    ELSE
-        SELECT 'Student failed' AS Result;
-    END IF;
+    -- Capture the number of affected rows
+    SET p_AffectedRows = ROW_COUNT();
 
-END //
+    -- In case no rows are affected, raise an error
+    IF p_AffectedRows = 0 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Transaction Error: No update performed.';
+    END IF;
+END$$
 
 DELIMITER ;
