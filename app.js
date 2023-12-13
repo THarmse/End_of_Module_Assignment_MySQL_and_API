dae5cbfae20f579a6e4c8b860d0d887a86ec8eca
TheodorHarmse <t.harmse@liverpool.ac.uk>
2023-12-13 20:10:46 +0400

    Theodor Harmse (Teddy) :  Removed View from loading in Browser and Disabled Browswer load, as this is purely an API.

--- app.js
+++ app.js
@@ -1,25 +1,18 @@
 'use strict';
 var debug = require('debug')('myapp:server');
 var express = require('express');
-var path = require('path');
+var path = require('path'); 
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 // Routes
-var indexRoute = require('./routes/index');
-//const userRoutes = require('./routes/user.routes');
-//const roleRoutes = require('./routes/role.routes');
 const courseRoutes = require('./routes/course.routes');
 const enrollmentRoutes = require('./routes/enrollment.routes');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-// uncomment after placing your favicon in /public
+// uncomment after placing favicon in /public
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -28,19 +21,14 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // API routes
-//app.use('/api/users', userRoutes);
-//app.use('/api/roles', roleRoutes);
 app.use('/api/courses', courseRoutes);
 app.use('/api/enrollments', enrollmentRoutes);
-
-// General routes
-app.use('/', indexRoute);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
-    next(err);
+    res.json({ error: err.message });
 });
 
 // error handlers
@@ -50,7 +38,7 @@ app.use(function (req, res, next) {
 if (app.get('env') === 'development') {
     app.use(function (err, req, res, next) {
         res.status(err.status || 500);
-        res.render('error', {
+        res.json({
             message: err.message,
             error: err
         });
@@ -61,7 +49,7 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function (err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
+    res.json({
         message: err.message,
         error: {}
     });
